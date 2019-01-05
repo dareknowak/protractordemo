fa2d7aafbec6edfdf62460d3732aae405cfb26da
nowad1 <darek.nowak@pega.com>
2019-01-05 18:15:44 +0100

    Added simple tests

---
+++ demo/conf.js
@@ -0,0 +1,11 @@
+exports.config = {
+  seleniumAddress: 'http://localhost:4444/wd/hub',
+  specs: ['todo-spec.js'],
+    capabilities: {
+		'browserName': 'chrome',
+		'platformName': 'desktop'
+			// 'chromeOptions': {
+   //  			'args': ['show-fps-counter=true']
+  	// 		}
+	},
+};
